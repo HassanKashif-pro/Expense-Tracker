8b6b39492e5e442f565991fccc3bf532e446e08a
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-10-30 20:06:33 +0500

    Basic Routing for the pages set-

--- client/my-react-app/src/App.tsx
+++ client/my-react-app/src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Expense from "./pages/Expense.tsx";
+import History from "./pages/History.tsx";
+import SignIn from "./pages/SignIn.tsx";
+import SignUp from "./pages/SignUp.tsx";
 
 function App() {
-  return <div>HELLOW</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/expense" element={<Expense />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
