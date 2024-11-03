257cf74b5c821f63bd3403ce95ab1131b407bd70
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-11-03 08:09:27 +0500

    Added the input forms in Sign In page-

--- my-react-app/src/pages/SignIn.tsx
+++ my-react-app/src/pages/SignIn.tsx
@@ -1,8 +1,44 @@
-import { Button } from "@/components/ui/button";
+"use client";
+
 import "../styles/SignIn.css";
-import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Invalid email address." }),
+  password: z
+    .string()
+    .min(8, { message: "Password must be at least 8 characters long." })
+    .max(20, { message: "Password cannot exceed 20 characters." }),
+});
 
 const SignIn = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
   return (
     <div className="Main-section">
       <div className="Side-section-wrapper">
@@ -43,6 +79,54 @@ const SignIn = () => {
           >
             <h1>Welcome</h1>
             <p>Start Managing Your Finances Today</p>
+          </div>
+          <div className="Register-Forms">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email Here</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="example@gmail.com"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8"
+              >
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password Here</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="at least 8 characters"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </form>
+            </Form>
           </div>
         </div>
       </div>
