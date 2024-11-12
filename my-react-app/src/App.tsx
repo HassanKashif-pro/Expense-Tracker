91e1d765578fe1cd4654bd2cba77cdd4bd631036
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-11-12 19:31:31 +0500

    (webapp): Icon for the user changes (static)

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -10,6 +10,7 @@ import Expense from "./pages/Expense";
 import History from "./pages/History";
 import Signin from "./pages/SignIn.tsx";
 import Signup from "./pages/SignUp.tsx";
+import Stats from "./pages/Stats.tsx";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/expense" element={<Expense />} />
         <Route path="/history" element={<History />} />
+        <Route path="/stats" element={<Stats />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<Navigate to="/signin" />} />
