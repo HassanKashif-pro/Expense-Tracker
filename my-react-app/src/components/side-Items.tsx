41b2166c3aab9d42ed8d76b97d06b986713e18a1
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-11-13 15:38:46 +0500

    (webapp): sidebar Items file set

---
+++ my-react-app/src/components/side-Items.tsx
@@ -0,0 +1,63 @@
+import { SidebarItem } from "@/components/SideBar";
+import Sidebar from "@/components/SideBar";
+import {
+  BarChart3,
+  Clock,
+  LayoutDashboard,
+  LifeBuoy,
+  Receipt,
+  Settings,
+} from "lucide-react";
+
+export default function IloveBalls() {
+  return (
+    <main className="App">
+      <Sidebar>
+        <SidebarItem
+          icon={<LayoutDashboard size={20} />}
+          text="Dashboard"
+          active={undefined}
+          link="/home"
+          alert={undefined}
+        />
+        <SidebarItem
+          icon={<BarChart3 size={20} />}
+          text="Statistics"
+          active
+          link="/stats"
+          alert={undefined}
+        />
+
+        <SidebarItem
+          icon={<Clock size={20} />}
+          text="History"
+          active={undefined}
+          link="/history"
+          alert={undefined}
+        />
+        <SidebarItem
+          icon={<Receipt size={20} />}
+          text="Expense"
+          active={undefined}
+          link="/expense"
+          alert={undefined}
+        />
+        <hr className="my-3" />
+        <SidebarItem
+          icon={<Settings size={20} />}
+          text="Settings"
+          active={undefined}
+          link="/settings"
+          alert={undefined}
+        />
+        <SidebarItem
+          icon={<LifeBuoy size={20} />}
+          text="Help"
+          active={undefined}
+          link="/help"
+          alert={undefined}
+        />
+      </Sidebar>
+    </main>
+  );
+}
