7e96c897d153a5e53213f7e41e36e1221c63f902
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-11-15 18:12:34 +0500

    (webapp)Header and other test elements added to the pages-

--- my-react-app/src/pages/History.tsx
+++ my-react-app/src/pages/History.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const History = () => {
-  return <div></div>;
+  return <div>This is the only history page you'll ever need.</div>;
 };
 
 export default History;
--- my-react-app/src/pages/Home.tsx
+++ my-react-app/src/pages/Home.tsx
@@ -1,5 +1,13 @@
-function Home() {
-  return <></>;
-}
+// Home.js
+import React from "react";
+import "../styles/Home.css";
 
-export default Home;
+export default function Home() {
+  console.log("Home component rendered"); // Check console output
+  return (
+    <div className="main-home">
+      <header className="header-main">THIS IS THE HEADER</header>
+      <div className="content">THIS IS THE TEXT</div>
+    </div>
+  ); // Basic element to confirm rendering
+}
