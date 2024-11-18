3790f912b86d6897b3bda482ee30cb06c3de6ac9
Hassan Kashif.pro <hassan.kashif.pro@gmail.com>
2024-11-18 20:33:52 +0500

    (webapp): Layout and style fixed in home and expense page.

--- my-react-app/src/components/ui/lineChart.tsx
+++ my-react-app/src/components/ui/lineChart.tsx
@@ -42,9 +42,6 @@ export function Component() {
     <Card>
       <CardHeader>
         <CardTitle>Area Chart - Gradient</CardTitle>
-        <CardDescription>
-          Showing total visitors for the last 6 months
-        </CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
@@ -110,18 +107,6 @@ export function Component() {
           </AreaChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter>
-        <div className="flex w-full items-start gap-2 text-sm">
-          <div className="grid gap-2">
-            <div className="flex items-center gap-2 font-medium leading-none">
-              Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
-            </div>
-            <div className="flex items-center gap-2 leading-none text-muted-foreground">
-              January - June 2024
-            </div>
-          </div>
-        </div>
-      </CardFooter>
     </Card>
   );
 }
